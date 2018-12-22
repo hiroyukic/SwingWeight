dd6c4eb566504051f09e158392a09e58497d7286
hiroyukic <hiroyukic@hotmail.com>
2018-12-22 16:12:53 +0900

    00

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,261 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend2 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series2 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabPage2.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.Panel1.Controls.Add(Me.ListBox1)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.ComboBox1)
+        Me.Panel1.Controls.Add(Me.TextBox2)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(5, 3)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(223, 525)
+        Me.Panel1.TabIndex = 1
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(5, 169)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(215, 352)
+        Me.ListBox1.TabIndex = 10
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.Button1.Location = New System.Drawing.Point(142, 107)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(70, 23)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Calc."
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(17, 30)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(68, 12)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Length Type"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(142, 27)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(70, 20)
+        Me.ComboBox1.TabIndex = 6
+        Me.ComboBox1.Text = "Normal"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(142, 82)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(70, 19)
+        Me.TextBox2.TabIndex = 4
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(142, 55)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(70, 19)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(3, 152)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(115, 12)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "SwingWeight [kg c㎡]"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(17, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(107, 12)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Balance [cm] 28-35"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(103, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Weight [g] 280-380"
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(234, 3)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(726, 525)
+        Me.TabControl1.TabIndex = 2
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.Chart1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(718, 499)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "TabPage1"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'Chart1
+        '
+        Me.Chart1.BackColor = System.Drawing.Color.LightSteelBlue
+        ChartArea2.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea2)
+        Legend2.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend2)
+        Me.Chart1.Location = New System.Drawing.Point(-4, 0)
+        Me.Chart1.Name = "Chart1"
+        Series2.ChartArea = "ChartArea1"
+        Series2.Legend = "Legend1"
+        Series2.Name = "Series1"
+        Me.Chart1.Series.Add(Series2)
+        Me.Chart1.Size = New System.Drawing.Size(722, 501)
+        Me.Chart1.TabIndex = 0
+        Me.Chart1.Text = "Chart1"
+        '
+        'TabPage2
+        '
+        Me.TabPage2.BackColor = System.Drawing.SystemColors.Window
+        Me.TabPage2.Controls.Add(Me.PictureBox2)
+        Me.TabPage2.Controls.Add(Me.PictureBox1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(718, 499)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "TabPage2"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(32, 232)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(401, 244)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(20, 8)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(484, 218)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(964, 533)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Swing Weight Calculator for Tennis rel.20180505"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabPage2.ResumeLayout(False)
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,354 @@
+﻿Imports System.Windows.Forms.DataVisualization.Charting
+
+Public Class Form1
+    Private dataTable As DataTable
+    Dim COM_BALANCE_POINT As String = "Balance Point [cm]"
+    Dim COM_WEIGHT0 As Double = 0
+    Dim COM_FREQ0 As Double
+    Dim COM_BP0 As Double
+    Dim COM_WEIGHT1 As Double = 0
+    Dim COM_FREQ1 As Double
+    Dim COM_BP1 As Double
+
+    ''' ------------------------------------------------------------------------
+    ''' <summary>
+    '''     「計算」ボタン押下 </summary>
+    ''' ------------------------------------------------------------------------
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '重量[g]
+        Dim weight As Double
+        '重心
+        Dim posg As Double
+        Try
+            weight = TextBox1.Text
+            posg = TextBox2.Text
+        Catch ex As Exception
+            Return
+        End Try
+
+        Try
+            'ＳＷライン削除
+            Chart1.ChartAreas(0).AxisY.StripLines.RemoveAt(0)
+        Catch ex As Exception
+        End Try
+
+        '振動周波数
+        Dim lengthType As String = ComboBox1.SelectedItem
+        Debug.WriteLine(lengthType)
+        Dim VibrationFreq As Double
+
+        If (lengthType = "Normal") Then
+            VibrationFreq = 1.38
+        Else
+            VibrationFreq = 1.4
+        End If
+
+        COM_WEIGHT1 = weight
+        COM_FREQ1 = VibrationFreq
+        COM_BP1 = posg
+
+        '=============================================================
+        Me.Chart1.Series.Clear() ' Series のクリア
+        Me.CreateData(COM_WEIGHT0, COM_BP0, COM_FREQ0, COM_WEIGHT1, COM_BP1, COM_FREQ1)
+        Me.CreateChart()
+        Me.Chart1.DataSource = Me.dataTable
+
+        '横線追加
+        Dim swingWeight As Double
+        swingWeight = getDataSet4SwingWeight(weight / 1000, posg, VibrationFreq)
+        AddHorizontalLine(Chart1.ChartAreas(0), swingWeight)
+
+        Dim message As String
+        message = "Length Type=" & lengthType
+        ListBox1.Items.Add(message)
+
+        message = "Weight=" & COM_WEIGHT1
+        ListBox1.Items.Add(message)
+
+        message = "Center=" & COM_BP1
+        ListBox1.Items.Add(message)
+
+        message = "SwingWeight=" & CStr(ToHalfAdjust(swingWeight, 1)) & " [kg.c㎡]"
+        ListBox1.Items.Add(message)
+
+        message = "-"
+        ListBox1.Items.Add(message)
+        message = " "
+        ListBox1.Items.Add(message)
+
+        If (swingWeight < 300 Or swingWeight > 440) Then
+            message = "計算結果が描画範囲を超えています。"
+            ListBox1.Items.Add(message)
+            message = "適正なパラメータを入力して下さい。"
+            ListBox1.Items.Add(message)
+            message = "-"
+            ListBox1.Items.Add(message)
+        End If
+
+        COM_WEIGHT0 = COM_WEIGHT1
+        COM_FREQ0 = COM_FREQ1
+        COM_BP0 = COM_BP1
+    End Sub
+
+
+    ''' ------------------------------------------------------------------------
+    ''' <summary>
+    '''     前回のパラメータでの計算値と今回のパラメータでの計算値を求める。</summary>
+    ''' <param name="weight0">
+    '''     前回の質量[g]</param>
+    ''' <param name="balance0">
+    '''     前回のバランスポイント[cm]</param>
+    ''' <param name="freq0">
+    '''     前回の振動周期[sec]</param>
+    ''' <param name="weight1">
+    '''     今回の質量[g]</param>
+    ''' <param name="balance1">
+    '''     今回のバランスポイント[cm]</param>
+    ''' <param name="freq1">
+    '''     今回の振動周期[sec]</param>
+    '''     
+    ''' ------------------------------------------------------------------------
+    Private Sub CreateData(weight0 As Double, balance0 As Double, freq0 As Double, weight1 As Double, balance1 As Double, freq1 As Double)
+        Me.dataTable = New DataTable("データ")
+        Me.dataTable.Columns.Add("BP", GetType(Double))
+        Me.dataTable.Columns.Add("SW0", GetType(Double))
+        Me.dataTable.Columns.Add("SW1", GetType(Double))
+
+        ' データの初期値を作成する
+        Dim swingWeight0 As Double
+        Dim swingWeight1 As Double
+
+        '--------------------------------------------------------------
+        ' データ作成
+        '--------------------------------------------------------------
+        For bp As Integer = 285 To 350 Step 5
+            Debug.Print(bp)
+            ' データを設定する
+            'Me.dataTable.Rows.Add(d, high, low, open, close, volume)
+            'Me.dataTable.Rows.Add(d, high, low, open, 0, volume)
+
+            ' 次に設定するデータを用意する
+            'd = d.AddDays(1)
+
+            If (weight0 = 0) Then
+                swingWeight0 = 300
+            Else
+                swingWeight0 = getDataSet4SwingWeight(weight0 / 1000, bp / 10, freq0)
+            End If
+
+            swingWeight1 = getDataSet4SwingWeight(weight1 / 1000, bp / 10, freq1)
+            Me.dataTable.Rows.Add(bp / 10, swingWeight0, swingWeight1)
+
+            Debug.Print(bp & " " & swingWeight0 & "  " & swingWeight1)
+        Next
+    End Sub
+
+
+    ''' ------------------------------------------------------------------------
+    ''' <summary>
+    '''  チャートを作る</summary>
+    ''' ------------------------------------------------------------------------
+    Private Sub CreateChart()
+
+        Me.Chart1.ChartAreas(0).AxisY.Maximum = 450
+        Me.Chart1.ChartAreas(0).AxisY.Minimum = 300
+        Me.Chart1.ChartAreas(0).AxisY.Interval = 20
+        Me.Chart1.ChartAreas(0).AxisY2.Maximum = 450
+        Me.Chart1.ChartAreas(0).AxisY2.Minimum = 300
+        Me.Chart1.ChartAreas(0).AxisY2.Interval = 20
+
+        ' グラフ１（今回）--------------------------------------------------------------------------
+        Dim swChart1
+        Try
+            swChart1 = Me.Chart1.Series.Add(COM_WEIGHT1.ToString)
+        Catch ex As Exception
+            swChart1 = Me.Chart1.Series.Add("Latest")
+        End Try
+
+        swChart1.XValueMember = "BP"
+        swChart1.YValueMembers = "SW1"
+
+        swChart1.ChartType = DataVisualization.Charting.SeriesChartType.Line ' (１) 縦棒
+        swChart1.YAxisType = DataVisualization.Charting.AxisType.Secondary ' (２) 参照する軸を分ける
+        swChart1.XValueType = DataVisualization.Charting.ChartValueType.Int32
+        swChart1.YValueType = DataVisualization.Charting.ChartValueType.Double
+        swChart1.ChartArea = Me.Chart1.ChartAreas(0).Name ' 同じ描画領域に描く
+        swChart1.Color = Color.Red
+        swChart1.Enabled = True
+
+        ' グラフ０ (前回）--------------------------------------------------------------------------
+        Dim swChart0
+        Try
+            swChart0 = Me.Chart1.Series.Add(COM_WEIGHT0.ToString)
+        Catch ex As Exception
+            swChart0 = Me.Chart1.Series.Add("Prev.")
+        End Try
+
+        swChart0.XValueMember = "BP"
+        swChart0.YValueMembers = "SW0"
+        swChart0.ChartType = DataVisualization.Charting.SeriesChartType.Line
+        swChart0.YAxisType = DataVisualization.Charting.AxisType.Primary ' (２) 参照する軸を分ける
+        swChart0.XValueType = DataVisualization.Charting.ChartValueType.Int32
+        swChart0.YValueType = DataVisualization.Charting.ChartValueType.Double
+        swChart0.ChartArea = Me.Chart1.ChartAreas(0).Name ' 同じ描画領域に描く
+        swChart0.Enabled = True
+
+    End Sub
+
+
+    ''' ------------------------------------------------------------------------
+    ''' <summary>
+    '''  フォーム初期化
+    '''  </summary>
+    ''' ------------------------------------------------------------------------
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        Me.CreateData(0, COM_BP0, COM_FREQ0, COM_WEIGHT1, COM_BP1, COM_FREQ1)
+        Me.CreateChart()
+        Me.Chart1.DataSource = Me.dataTable
+
+        'コンボボックス　Normal/Long
+        With ComboBox1
+            .Items.Add("Normal")
+            .Items.Add("Long")
+            .Font = New Font("ＭＳ Ｐゴシック", 8)
+            .Text = "Normal" 'テキストボックスに最初に表示して置く項目を設定
+        End With
+
+        'タブ名称
+        Me.TabPage1.Text = "S.W. Graph"
+        Me.TabPage2.Text = "Readme"
+
+        Me.Chart1.Series.Clear() ' (４) Series のクリアは最初だけ
+        'Me.CreateData()
+        Me.CreateChart()
+        Me.Chart1.DataSource = Me.dataTable
+        'Me.Chart1.ChartAreas(0).AxisY2.Maximum = 400 ' (２) 有効にすると、棒グラフが下がる
+
+        With Chart1.ChartAreas(0).AxisY
+            '目盛りの設定と目盛線の設定
+            .Maximum = 450    '点数の最大値
+            .Minimum = 300      '点数の最小値
+            .Interval = 20    '点数のメモリ間隔(２０点毎)
+            '"#,###" と設定すると基点の 0 が表示されない。
+            .LabelStyle.Format = "#,##0"  '桁区切りで表示の場合
+
+            '補助線を表示
+            '.MinorGrid.Enabled = True  'True に設定しないと表示しない
+
+            'ラベルに対して自動的に適用できるスタイルの設定
+            .LabelAutoFitMaxFontSize = 10
+
+            'Y軸のラベルとプロットエリアとの境界の縦線
+            .LineWidth = 1
+            .LineColor = Color.Blue
+            .LineDashStyle = DataVisualization.Charting.ChartDashStyle.Solid
+
+            'Y軸のタイトル関係の設定
+            .Title = "Swing Weight [kg c㎡]"
+            .TitleAlignment = StringAlignment.Center        '中央に表示
+            '.TextOrientation = TextOrientation.Stacked      '縦書きで表示
+            '.TitleFont = New Font("ＭＳ Ｐ明朝", 10, FontStyle.Regular)
+        End With
+
+        With Chart1.ChartAreas(0).AxisX
+            .Interval = 1   '点数のメモリ間隔(２０点毎)
+            '"#,###" と設定すると基点の 0 が表示されない。
+            .LabelStyle.Format = "#,##0"  '桁区切りで表示の場合
+
+            '補助線
+            .MinorGrid.Enabled = True  'True に設定しないと表示しない
+            .MinorGrid.Interval = 2
+
+            'ラベルスタイルの設定
+            .LabelAutoFitMaxFontSize = 10
+
+            'Y軸のラベルとプロットエリアとの境界の縦線
+            .LineWidth = 1.2
+            .LineColor = Color.Blue
+            .LineDashStyle = DataVisualization.Charting.ChartDashStyle.Solid
+
+            'Y軸のタイトル関係の設定
+            .Title = COM_BALANCE_POINT
+
+            .TitleAlignment = StringAlignment.Center        '中央に表示
+            '.TextOrientation = TextOrientation.Stacked      '縦書きで表示
+            '.TitleFont = New Font("ＭＳ Ｐ明朝", 10, FontStyle.Regular)
+        End With
+
+    End Sub
+
+
+    ''' ------------------------------------------------------------------------
+    ''' <summary>
+    '''     スイングウエイトの算出関数</summary>
+    ''' <param name="weight">
+    '''     質量[g]</param>
+    ''' <param name="glength">
+    '''     バランスポイント[cm]</param>
+    ''' <param name="cycleTime">
+    '''     振動周期[sec]</param>  
+    ''' ------------------------------------------------------------------------
+    Private Function getDataSet4SwingWeight(weight As Double, glength As Double, cycleTime As Double) As Double
+        Dim ds As New DataSet
+        Dim dt As New DataTable
+
+        Dim Ig1 As Double
+        Dim Ig2 As Double
+
+        Ig1 = (weight * 980 * glength * (cycleTime ^ 2)) / (4 * (3.1415 ^ 2))
+        Ig2 = weight * (glength ^ 2)
+
+        '慣性モーメント　Ig = Ig1 - Ig2
+        Dim Ig As Double
+        Ig = Ig1 - Ig2
+
+        '慣性モーメント（軸まわり）　Io = Ig + Lg^2 * M
+        Dim Io As Double
+        Io = Ig + ((glength - 7.5) ^ 2) * weight
+        System.Diagnostics.Debug.WriteLine(Io)
+
+        Return (Io)
+    End Function
+
+
+    ''' ------------------------------------------------------------------------
+    ''' <summary>
+    '''     指定した精度の数値に四捨五入</summary>
+    ''' <param name="dValue">
+    '''     丸め対象の倍精度浮動小数点数。</param>
+    ''' <param name="iDigits">
+    '''     戻り値の有効桁数の精度。</param>
+    ''' <returns>
+    '''     iDigits に等しい精度の数値に四捨五入された数値。</returns>
+    ''' ------------------------------------------------------------------------
+    Public Shared Function ToHalfAdjust(ByVal dValue As Double, ByVal iDigits As Integer) As Double
+        Dim dCoef As Double = System.Math.Pow(10, iDigits)
+
+        If dValue > 0 Then
+            Return System.Math.Floor((dValue * dCoef) + 0.5) / dCoef
+        Else
+            Return System.Math.Ceiling((dValue * dCoef) - 0.5) / dCoef
+        End If
+    End Function
+
+
+
+    ''' ------------------------------------------------------------------------
+    ''' <summary>
+    ''' 指定したチャートエリアの指定したy座標に赤い横線を追加するメソッド</summary>
+    ''' ------------------------------------------------------------------------
+    Private Sub AddHorizontalLine(area As ChartArea, yValue As Double)
+        Dim line As StripLine = New StripLine()
+
+        line.BorderColor = Color.Red
+        line.IntervalOffset = yValue
+        line.Interval = 0
+        line.BorderWidth = 1
+
+        line.BorderDashStyle = DataVisualization.Charting.ChartDashStyle.Dash
+
+        area.AxisY.StripLines.Add(line)
+
+    End Sub
+
+End Class
